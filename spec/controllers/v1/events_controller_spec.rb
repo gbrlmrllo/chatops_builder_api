b6d446ef60cf7b904a3fd654c0d0a0918b9b306b
Anyelo Petit <anyelopetit@gmail.com>
2020-05-10 20:06:51 -0400

    refactorized spec/controllers/v1/events_controller_spec.rb

--- spec/controllers/v1/events_controller_spec.rb
+++ spec/controllers/v1/events_controller_spec.rb
@@ -3,51 +3,41 @@
 require "rails_helper"
 
 RSpec.describe V1::EventsController, type: :controller do
-  context "with an invalid token" do
-    describe "POST #consume" do
+  describe "POST #consume" do
+    let(:app) { create(:app, :with_event_schema) }
+    let(:token) { app.credential.token }
+    let(:params) { nil }
+
+    before do
+      request.headers["AppToken"] = token
+      post :consume, params: params
+    end
+
+    context "with an invalid token" do
+      let(:token) { "iNv4lidT0k3n" }
+
       it "404 - Not Found" do
-        request.headers["AppToken"] = "iNv4lidT0k3n"
-        post :consume, params: { }
         expect(response.status).to eq(404)
       end
     end
-  end
 
-  context "with a valid token" do
-    context "with an invalid event schema name" do
-      let(:params) { { name: "Invalid" } }
-      let(:credential) { create(:credential) }
+    context "with a valid token" do
+      context "with an invalid event schema name" do
+        let(:params) { { name: "Invalid" } }
 
-      describe "POST #consume" do
         it "404 - Not Found" do
-          request.headers["AppToken"] = credential.token
-          post :consume, params: params
           expect(response.status).to eq(404)
         end
       end
-    end
 
-    context "with blank fields" do
-      let(:credential) { create(:credential) }
-
-      describe "POST #consume" do
+      context "with blank fields" do
         it "404 - Not Found" do
-          request.headers["AppToken"] = credential.token
-          post :consume, params: { }
           expect(response.status).to eq(404)
         end
       end
-    end
 
-    context "with a valid event schema name" do
-      let(:app) { create(:app, :with_event_schema) }
-      let(:params) { { name: app.event_schemas.take.name } }
-
-      describe "POST #consume" do
-        before do
-          request.headers["AppToken"] = app.credential.token
-          post :consume, params: params
-        end
+      context "with a valid event schema name" do
+        let(:params) { { name: app.event_schemas.take.name } }
 
         it "201 - Created" do
           expect(response.status).to eq(201)
